42be442d3142478e27150a61c8c28f107358ccb7
Hannah <hannahjmccain@gmail.com>
2017-01-11 15:48:49 -0500

    Outlined problem #3

--- searchingRewrite.js
+++ searchingRewrite.js
@@ -1,13 +1,38 @@
+// Given a number N, guess N without guessing higher than N more than once.
+// Basically: binary search until you guess over, then linear search
+// Split in half, if higher, linear search up to halfway point
+// If lower, split in half again (etc.)
+
+// 80
+// 50 > 75 > 87
+// 75 - 100
+
+const findNum = (num, currentValue, start = 1, end = 100) => {
+
+	if (currentValue < num) {
+
+	} else {
+		// linear search
+	}
+
+}
+
+// Write an algorithm for finding a book using Dewey decimal system
+// One section
+// One shelf
+// 1 of each number
+// Cruel friend asks you to find the book she's guessing
+// You create an algorithm to find the damn book
+
+
 const stocks = (ary, max = 0, start = 0) => {
 	let profit = max;
 
-	if (ary.length === 0) {
-		return profit;
-	}
+	if (ary.length === 0) return profit;
 
-	for (let i = 0; i < ary.length; i++) {
-		if ((ary[i] - ary[0]) > profit) {
-			profit = ary[i] - ary[0];
+	for (val of ary) {
+		if ((val - ary[0]) > profit) {
+			profit = val - ary[0];
 		}
 	}
 
